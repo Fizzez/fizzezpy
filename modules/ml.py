693c78c529c73ea26dc317ff4b46389cf20eceb1
WangYudong <yudong.wang.chn@gmail.com>
2020-09-20 13:25:46 +0900

    [add] .gitignore

---
+++ modules/ml.py
@@ -0,0 +1,23 @@
+
+def get_logger(out_file=None):
+    import logging
+
+    logger = logging.getLogger()
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    logger.handlers = []
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    if out_file is not None:
+        fh = logging.FileHandler(out_file)
+        fh.setFormatter(formatter)
+        fh.setLevel(logging.INFO)
+        logger.addHandler(fh)
+    logger.info('logger set up')
+
+    return logger
+
